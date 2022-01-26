12782b69741a4f631411716cf0fc6b5ce36512f5
Pablo <alejozx456@gmail.com>
2022-01-26 18:52:49 -0500

    19h00

---
+++ app/src/main/java/example/com/prueba2_pablo_alvear/ResultsDialogFragment.java
@@ -0,0 +1,45 @@
+package example.com.prueba2_pablo_alvear;
+import android.os.Bundle;
+
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
+import android.util.Log;
+public class ResultsDialogFragment extends DialogFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final QuizViewModel quizViewModel = ViewModelProviders.of((FragmentActivity) getActivity()).get(QuizViewModel.class);
+        int totalGuesses = quizViewModel.getTotalGuesses();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(
+                getString(R.string.results, totalGuesses, (1000 / (double) totalGuesses)));
+
+        builder.setPositiveButton(R.string.reset_quiz, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try{
+                    MainActivityFragment quizFragment = (MainActivityFragment) getParentFragment();
+                    try{
+                        quizFragment.resetQuiz();
+                    }catch (Exception e){
+                        Log.e(quizViewModel.getTag(),"Unable to call resetQuiz()", e);
+                    }
+                }
+                catch (Exception e){
+                    Log.e(quizViewModel.getTag(),"Unable to get ActivityMainFragment", e);
+                }
+            }
+        });
+        return builder.create();
+    }
+}
---
+++ app/src/main/java/example/com/prueba2_pablo_alvear/SettingsActivityFragment.java
@@ -0,0 +1,13 @@
+package example.com.prueba2_pablo_alvear;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import androidx.annotation.Nullable;
+
+public class SettingsActivityFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
